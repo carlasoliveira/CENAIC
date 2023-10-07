8b7780251db90b26e108ed2be4696ba668aa3582
Carla Oliveira <54954245+carlasoliveira@users.noreply.github.com>
2023-10-07 14:31:27 -0300

    Update media-alunos.py

--- media-alunos.py
+++ media-alunos.py
@@ -1,12 +1,17 @@
 print("Insira o nome do aluno: ")
 nomeDoUsuário = input()
-print("Insira a nota 1: ")
-nota1 = float(input())
-print("Insira a nota 2: ")
-nota2 = float(input())
-print("Insira a nota 3: ")
-nota3 = float(input())
-media = (nota1+nota2+nota3)/3
+
+notas = [0,0,0] 
+
+for contador in range(3):
+    print("Insira a nota ", (contador+1), ": ")
+    while True:
+        notas[contador] = float(input())
+        'print("Valor negativo. Insira uma nota válida!")'
+        if(notas[contador]>0):
+            break
+
+media = (notas[0]+notas[1]+notas[2])/3
 
 print("Nome do aluno: ", nomeDoUsuário)
 print("Média do aluno: ", media)
